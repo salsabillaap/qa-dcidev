d5d4711d9e820a4a39e50ddbc71d5caebf2bd022
wahyunusantaraazis <wahyunusantaraazis@gmail.com>
2021-12-13 22:51:03 +0700

    add endpoint signup

--- src/main/java/starter/BaseEndpoint/BaseEndpoint.java
+++ src/main/java/starter/BaseEndpoint/BaseEndpoint.java
@@ -3,4 +3,5 @@ package starter.BaseEndpoint;
 public class BaseEndpoint {
     public String BASEURL = "https://barengin.site";
     public String Login = BASEURL+"/login";
+    public String Signup = BASEURL+"/signup";
 }
