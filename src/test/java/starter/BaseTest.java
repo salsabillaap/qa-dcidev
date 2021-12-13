53ecbb603a9dd321bd458f577b551b85649fca9d
wahyunusantaraazis <wahyunusantaraazis@gmail.com>
2021-12-13 22:51:34 +0700

    add object signup

--- src/test/java/starter/BaseTest.java
+++ src/test/java/starter/BaseTest.java
@@ -1,7 +1,9 @@
 package starter;
 
 import starter.Login.Login;
+import starter.Signup.Signup;
 
 public class BaseTest {
     public Login login = new Login();
+    public Signup signup = new Signup();
 }
