e208ed0d0cd110c16cc0cee9afc1407a03f5b1f6
wahyunusantaraazis <wahyunusantaraazis@gmail.com>
2021-12-13 14:51:35 +0700

    add steps validate respon

--- src/test/java/starter/steps/loginSteps.java
+++ src/test/java/starter/steps/loginSteps.java
@@ -8,26 +8,34 @@ import starter.BaseTest;
 
 public class loginSteps extends BaseTest {
 
-    @Given("I am set valid body request email {string} and password {string}")
-    public void iAmSetValidBodyRequestEmailAndPassword(String email, String password) {
-        login.setBodyRequest(email,password);
+    @Given("I am set body request email {string} and password {string}")
+    public void iAmSetBodyRequestEmailAndPassword(String email, String password) {
+        login.setRequestPayload(email,password);
     }
 
     @And("I am set valid header {string} with fill {string}")
     public void iAmSetValidHeaderWithFill(String header, String content) {
+        //login.setBodyRequest();
         login.setHeader(header,content);
     }
 
     @When("I am hit endpoint")
     public void iAmHitEndpoint() {
-        login.hitEndpointAuthentication();
+        login.hitEndpointLogin();
     }
 
-    @Then("I am get a status code {int} and valid response")
-    public void iAmGetAStatusCodeOKAndValidResponse(int statuscode) {
-        login.validateEndpointAuthentication(statuscode);
+    @Then("I am get a status code {int}")
+    public void iAmGetAStatusCode(int statuscode) {
+        login.validateEndpointLogin(statuscode);
     }
 
+    @And("valid response success")
+    public void validResponseSuccess() {
+        login.JsonschemaEndpointLoginSuccess();
+    }
 
-
+    @And("valid response failed")
+    public void validResponseFailed() {
+        login.JsonschemaEndpointLoginFailed();
+    }
 }
