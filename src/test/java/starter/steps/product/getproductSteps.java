5e8ab46a5af6af20a0a8ac0d9423babf59428b5b
wahyunusantaraazis <wahyunusantaraazis@gmail.com>
2021-12-16 14:12:13 +0700

    update test GET Product

--- src/test/java/starter/steps/product/getproductSteps.java
+++ src/test/java/starter/steps/product/getproductSteps.java
@@ -4,23 +4,63 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import starter.BaseTest;
 
-public class getproductSteps {
+public class getproductSteps extends BaseTest {
 
     @Given("I am set header {string} with value {string}")
-    public void iAmSetHeaderWithValue(String arg0, String arg1) {
+    public void iAmSetHeaderWithValue(String header, String content) {
+        getproduct.setHeader(header,content);
     }
 
     @When("I am hit endpoint get all product")
     public void iAmHitEndpointGetAllProduct() {
+        getproduct.hitEndpointGetProduct();
+    }
+    //hit invalid endpoint
+    @When("I am hit invalid endpoint get all product")
+    public void iAmHitInvalidEndpointGetAllProduct() {
+        getproduct.hitInvalidEndpointGetProduct();
     }
 
     @Then("I am get a status code {int} for get all product")
-    public void iAmGetAStatusCodeForGetAllProduct(int arg0) {
+    public void iAmGetAStatusCodeForGetAllProduct(int statuscode) {
+        getproduct.validateEndpointGET(statuscode);
     }
 
-    @And("validate response success get all product")
-    public void validateResponseSuccessGetAllProduct() {
+    @And("validate response body code {int} message {string} for get all product")
+    public void validateResponseBodyCodeMessageForGetAllProduct(int code, String message) {
+        getproduct.JsonschemaGetAllProductSuccess(code,message);
     }
 
+    @And("validate response body failed code {int} message {string} for get all product")
+    public void validateResponseBodyFailedCodeMessageForGetAllProduct(int code, String message) {
+        getproduct.JsonschemaGetAllProductFailed(code,message);
+    }
+
+    //GET Product By ID
+    @When("I am hit endpoint get product by id {int}")
+    public void iAmHitEndpointGetProductById(int id) {
+        getproduct.hitEndpointGetProductID(id);
+    }
+    //GET Product By Invalid ID
+    @When("I am hit endpoint get product by id {string}")
+    public void iAmHitEndpointGetProductById(String invalidID) {
+        getproduct.hitEndpointGetProductInvalidID(invalidID);
+    }
+
+    @Then("I am get a status code {int} for get product by id")
+    public void iAmGetAStatusCodeForGetProductById(int statuscode) {
+        getproduct.validateEndpointGET(statuscode);
+    }
+
+    @And("validate response body code {int} message {string} for get product by id {int}")
+    public void validateResponseBodyCodeMessageForGetProductById(int code, String message, int id) {
+        getproduct.JsonschemaGetProductByIDSuccess(code,message,id);
+    }
+
+    @And("validate response body failed code {int} message {string} for get product by id")
+    public void validateResponseBodyFailedCodeMessageForGetProductById(int code, String message) {
+        getproduct.JsonschemaGetProductByIDFailed(code,message);
+    }
 }
--- src/test/resources/features/product/getproduct.feature
+++ src/test/resources/features/product/getproduct.feature
@@ -1,12 +1,40 @@
+@GETproduct
 Feature: GET product
-  fdsafd
-  fafa
-  fdsaf
+  As a user
+  I want to see all product and details product
+  So that I am success get product
 
+  @TC_6 #SUCCESS
   Scenario: I want to see list all product
     Given I am set header "Content-Type" with value "applicaton/json"
     When I am hit endpoint get all product
     Then I am get a status code 200 for get all product
-    And validate response success get all product
+    And validate response body code 200 message "Success Operation" for get all product
 
-  Scenario:
+  @TC_7 #FAILED #tanyakan ke BE mengenai kondisi data not found dan bad request
+  Scenario: I want to see all product with invalid endpoint
+    Given I am set header "Content-Type" with value "applicaton/json"
+    When I am hit invalid endpoint get all product
+    Then I am get a status code 400 for get all product
+    And validate response body failed code 400 message "Data Not Found" for get all product
+
+  @TC_8 #SUCCESS
+  Scenario: I want to see details product by id
+    Given I am set header "Content-Type" with value "applicaton/json"
+    When I am hit endpoint get product by id 10
+    Then I am get a status code 200 for get product by id
+    And validate response body code 200 message "Success Operation" for get product by id 10
+
+  @TC_9 #FAILED
+  Scenario: I want to see details product by id not found
+    Given I am set header "Content-Type" with value "applicaton/json"
+    When I am hit endpoint get product by id 100
+    Then I am get a status code 400 for get product by id
+    And validate response body failed code 400 message "Data Not Found" for get product by id
+
+  @TC_10 #FAILED
+  Scenario: I want to see details product by invalid id
+    Given I am set header "Content-Type" with value "applicaton/json"
+    When I am hit endpoint get product by id "satu"
+    Then I am get a status code 400 for get product by id
+    And validate response body failed code 400 message "Invalid Id" for get product by id
