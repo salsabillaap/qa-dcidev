404387226a8e30452676319a1de1fdae1beac4e2
wahyunusantaraazis <wahyunusantaraazis@gmail.com>
2021-12-16 14:12:57 +0700

    update

--- src/test/java/starter/BaseTest.java
+++ src/test/java/starter/BaseTest.java
@@ -1,6 +1,7 @@
 package starter;
 
 import starter.Login.Login;
+import starter.Product.GetProduct;
 import starter.Product.PostProduct;
 import starter.Signup.Signup;
 
@@ -8,4 +9,5 @@ public class BaseTest {
     public Login login = new Login();
     public Signup signup = new Signup();
     public PostProduct postproducts = new PostProduct();
+    public GetProduct getproduct = new GetProduct();
 }
